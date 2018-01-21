55c50e70e37fc0c7cb3081b7343ab9599da03ed5
Kim Maida <kim.maida@auth0.com>
2018-01-21 17:19:58 -0500

    Add prod environment sample and web server.

---
+++ server.js
@@ -0,0 +1,24 @@
+// Modules
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+// App
+const app = express();
+app.use(cors());
+
+// Run the app with static files from the /dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back /dist/index.html
+// This passes routing to the Angular app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Set port
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+// Start the app
+app.listen(port, () => console.log(`Angular app deployed at localhost:${port}`));
